f4050f2f251df25c6a3795d59b127ce5dadd29c4
tuanpham <tuan.phamhoang.2506@gmail.com>
2018-04-13 21:18:22 +0700

    Finish basic Master detail page

--- SmartHotel/SmartHotel/Views/MenuView.xaml.cs
+++ SmartHotel/SmartHotel/Views/MenuView.xaml.cs
@@ -9,12 +9,44 @@ using Xamarin.Forms.Xaml;
 
 namespace SmartHotel.Views
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class MenuView : ContentPage
-	{
-		public MenuView ()
-		{
-			InitializeComponent ();
-		}
-	}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MenuView : ContentPage
+    {
+        public MenuView()
+        {
+            InitializeComponent();
+        }
+        private void OnBookingMenuTapped(object sender, EventArgs args)
+        {
+            this.NavigationToPage(new BookingView());
+        }
+        private void OnMyRoomMenuTapped(object sender, EventArgs args)
+        {
+            this.NavigationToPage(new MyRoomView());
+        }
+        private void OnSuggestionMenuTapped(object sender, EventArgs args)
+        {
+            this.NavigationToPage(new SuggesstionsView());
+        }
+        private void OnConciergeMenuTapped(object sender, EventArgs args)
+        {
+            this.NavigationToPage(new ConciergeView());
+        }
+        private void OnLogoutMenuTapped(object sender, EventArgs args)
+        {
+            App.Current.MainPage = new LoginView();
+        }
+
+        private void NavigationToPage(ContentPage page)
+        {
+            if (Application.Current.MainPage is MainView mainView)
+            {
+                if (mainView.Detail is NavigationPage navigationPage)
+                {
+                    navigationPage.PushAsync(page);
+                    mainView.IsPresented = false;
+                }
+            }
+        }
+    }
 }
